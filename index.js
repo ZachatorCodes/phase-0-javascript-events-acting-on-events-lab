28461e9fab1574a0cb9d63dfde026740acda67fa
Zach <zachatorcodes@gmail.com>
2022-10-26 14:17:00 -0600

    Done.

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ function moveDodgerLeft() {
     const left = parseInt(leftNumbers, 10);
   
     if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
+      dodger.style.left = `${left - 5}px`;
     }
 }
 
 function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
-  
+   
     if (left > 0) {
-      dodger.style.left = `${left + 1}px`;
+      dodger.style.left = `${left + 5}px`;
     }
 }
 
